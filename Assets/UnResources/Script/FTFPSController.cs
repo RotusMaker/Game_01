bd7a0b1c664f9576b294242beebaf1be40bfc64a
Rotus Maker <RotusMaker@gmail.com>
2016-10-06 00:15:06 +0900

    PROJECT CREATE.

---
+++ Assets/UnResources/Script/FTFPSController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
+using Random = UnityEngine.Random;
+
+namespace UnityStandardAssets.Characters.FirstPerson
+{
+	//first person controller rapper class.
+	public class FTFPSController : FirstPersonController {
+	}
+}
---
+++ Assets/UnResources/Script/FTGlobal.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class FTGlobal {
+	//[System Info]
+	public static string GAME_NAME = "";
+	public static string GAME_VERSION = "";
+
+	//[Paths]
+	public static string PATH_PATTERN_PREFAB = "";
+	public static string PATH_EASY_PATTERN = PATH_PATTERN_PREFAB + "";
+	public static string PATH_NORMAL_PATTERN = PATH_PATTERN_PREFAB + "";
+	public static string PATH_HARD_PATTERN = PATH_PATTERN_PREFAB + "";
+
+	public static string getPatternPrefabPath(int gameLevel, int boxId)
+	{
+		return "";
+	}
+
+	//[Game Info]
+	public static int nGameScore = 0;
+
+
+	//[Utils]
+	public static void SaveGameData()
+	{
+		//PlayerPrefs.SetString ();
+	}
+}
