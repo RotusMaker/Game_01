1d73bf23761837d2292942744a4aa5dfa64252c3
RotusMakerjy <RotusMakerjy@gmail.com>
2016-10-31 21:39:40 +0900

    Asset store

---
+++ Assets/UnResources/Prefab/DH/MC.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MC : MonoBehaviour {
+	public float z = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate (0, 0, -z);
+	
+	}
+}
