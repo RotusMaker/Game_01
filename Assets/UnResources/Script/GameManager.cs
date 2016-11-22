eb80339ccf35c4be5423f4027a32261d9a315d89
Rotus Maker <RotusMaker@gmail.com>
2016-11-22 22:27:05 +0900

    dwdawddw

--- Assets/UnResources/Script/GameManager.cs
+++ Assets/UnResources/Script/GameManager.cs
@@ -1,16 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour 
+public class GameManager : MonoSingleton<GameManager>
 {
+	public enum eGameState
+	{
+		None,
+		Loading,
+		Ready,
+		Start,
+		Play,
+		Result
+	}
+	private eGameState m_gameState = eGameState.None;
 
-	// Use this for initialization
-	void Start () {
-	
+	public eGameState GetGameState()
+	{
+		return m_gameState;
+	}
+	public void SetState(eGameState state)
+	{
+		if (m_gameState != state) 
+		{
+			EndState (m_gameState);
+			m_gameState = state;
+			EnterState (m_gameState);
+		}
+	}
+
+	void Start () 
+	{
+		LoadPrefabManager.instance.LoadPrefab ("r", 0);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		DoState (GetGameState ());
+	}
+
+	private void EnterState(eGameState state)
+	{
+	}
+
+	private void DoState(eGameState state)
+	{
+	}
+
+	private void EndState(eGameState state)
+	{
 	}
 }
---
+++ Assets/UnResources/Script/LoadPrefabManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LoadPrefabManager : MonoSingleton<LoadPrefabManager>
+{
+	public float m_fCurrentProgress = 0f;
+	private Dictionary<string,GameObject> m_dicPrefabs = new Dictionary<string, GameObject> ();
+
+	void Start()
+	{
+		m_dicPrefabs.Clear();
+	}
+
+	public void LoadPrefab(string sectionName, int stageNumber)
+	{
+		string prefabName = string.Format ("{0}_{1}",sectionName,stageNumber);
+		StartCoroutine (LoadingPrefab (prefabName));
+	}
+
+	IEnumerator LoadingPrefab(string prefabName)
+	{
+		string prefabPath = string.Format ("Prefab/Pattern/{0}",prefabName);
+
+		ResourceRequest req = Resources.LoadAsync<GameObject> (prefabPath);
+		while (!req.isDone) 
+		{
+			m_fCurrentProgress = req.progress * 100f;
+			yield return null;
+		}
+
+		GameObject prefabObj = req.asset as GameObject;
+		m_dicPrefabs.Add (prefabName, prefabObj);
+	}
+}
