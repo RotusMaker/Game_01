c29c8e5558b8365a27ee3191f4cafef983bea9e0
RotusMakerJB <RotusMakerJB@gmail.com>
2016-11-23 16:33:04 +0900

    trigger 기능 추가.

---
+++ Assets/Editor/TriggerBoxEditor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomEditor(typeof(TriggerBox))]
+public class TriggerBoxEditor : Editor {
+
+	TriggerBox myTarget;
+	private ReorderableList list;
+
+	private void OnEnable()
+	{
+		myTarget = (TriggerBox)target;
+		list = new ReorderableList(serializedObject, serializedObject.FindProperty("m_listTriggingData"), true, true, true, true);
+
+		list.elementHeight = EditorGUIUtility.singleLineHeight * 2f;
+
+		list.drawHeaderCallback = (Rect rect) => {
+			EditorGUI.LabelField(rect, "Bound");
+		};
+
+		list.drawElementCallback =  
+			(Rect rect, int index, bool isActive, bool isFocused) => {
+			var element = list.serializedProperty.GetArrayElementAtIndex(index);
+			rect.y += 2;
+			EditorGUI.PropertyField(
+				new Rect(rect.x, rect.y, rect.width/2f, EditorGUIUtility.singleLineHeight),
+				element.FindPropertyRelative("target"), GUIContent.none);
+			EditorGUI.PropertyField(
+				new Rect(rect.x + rect.width/2f, rect.y, rect.width/2f, EditorGUIUtility.singleLineHeight),
+				element.FindPropertyRelative("method"), GUIContent.none);
+		};
+	}
+
+	public override void OnInspectorGUI() 
+	{
+		EditorGUILayout.BeginVertical ("box");
+		serializedObject.Update();
+		list.DoLayoutList();
+		serializedObject.ApplyModifiedProperties();
+		EditorGUILayout.EndVertical ();
+	}
+}
---
+++ Assets/UnResources/Script/TriggerBox.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent (typeof (BoxCollider))]
+public class TriggerBox : MonoBehaviour 
+{
+	public enum eTriggerType
+	{
+		Disable = 0,
+		Go,
+		Cam_Rotation,
+	}
+	
+	[System.Serializable]
+	public struct TriggingData
+	{
+		public GameObject target;
+		public eTriggerType method;
+	}
+	public TriggingData[] m_listTriggingData;
+	private BoxCollider m_boxCollider;
+
+	void Start()
+	{
+		m_boxCollider = this.GetComponent<BoxCollider>();
+		m_boxCollider.isTrigger = true;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (m_listTriggingData != null && m_listTriggingData.Length > 0)
+		{
+			for(int i=0; i<m_listTriggingData.Length; i++)
+			{
+				string method = GetMethodName(m_listTriggingData[i].method);
+				if (string.IsNullOrEmpty(method))
+				{
+					continue;
+				}
+				m_listTriggingData[i].target.SendMessage(method);
+			}
+		}
+	}
+
+	string GetMethodName(eTriggerType type)
+	{
+		switch(type)
+		{
+		case eTriggerType.Go:	return "Go";
+		default: return string.Empty;
+		}
+	}
+}
