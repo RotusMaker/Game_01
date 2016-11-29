8cf486b44d4c59e0cede2ad40a8eb4b36933b378
Rotus Maker <RotusMaker@gmail.com>
2016-11-29 23:11:06 +0900

    dwdwd

---
+++ Assets/FOVAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FOVAnimation : MonoBehaviour {
+
+	IEnumerator Start () 
+	{
+		Camera cam = this.GetComponent<Camera> ();
+
+		while (true) 
+		{
+			cam.fieldOfView -= Time.deltaTime * 0.2f;
+			yield return null;
+		}
+	}
+}
