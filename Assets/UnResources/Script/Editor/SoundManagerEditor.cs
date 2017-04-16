a73de4eb6053d08a664637a831a3d3349be90062
Rotus Maker <RotusMaker@gmail.com>
2017-04-16 19:39:10 +0900

    sound manager

---
+++ Assets/UnResources/Script/Editor/SoundManagerEditor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(SoundManager))]
+public class SoundManagerEditor : Editor {
+
+	SoundManager myTarget;
+
+	void OnEnable()
+	{
+		myTarget = target as SoundManager;
+	}
+	public override void OnInspectorGUI()
+	{
+		myTarget.m_Root = (GameObject)EditorGUILayout.ObjectField("Camera Object", myTarget.m_Root, typeof(GameObject), true);
+		EditorGUILayout.BeginVertical ("box");
+		GUILayout.Label (myTarget.m_updateDate);
+		EditorGUILayout.EndVertical ();
+		if (GUILayout.Button ("Update")) {
+			if (Linking ()) {
+				myTarget.m_updateDate = System.DateTime.Now.ToString ();
+			}
+			else {
+				myTarget.m_updateDate = "Link Error";
+			}
+		}
+	}
+
+	public bool Linking()
+	{
+		if (myTarget.m_dicAudio == null) {
+			myTarget.m_dicAudio = new System.Collections.Generic.Dictionary<string, AudioSource>();
+		}
+		FindClassChild (myTarget.m_Root.transform);
+		return true;
+	}
+
+	public void FindClassChild(Transform root)
+	{
+		for (int i = 0; i < root.childCount; i++) {
+			Transform child = root.GetChild (i);
+			if (child.GetComponent<AudioSource>()) {
+				Debug.Log (string.Format("Add Audio: {0}",child.name));
+				child.GetComponent<AudioSource>().playOnAwake = false;
+				myTarget.m_dicAudio.Add (child.name, child.GetComponent<AudioSource>());
+			}
+		}
+	}
+}
---
+++ Assets/UnResources/Script/SoundManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eSoundPlayType
+{
+	OneShot,
+	Loop,
+}
+
+public class SoundManager : MonoBehaviour {
+
+	public GameObject m_Root;
+	public string m_updateDate;
+	public Dictionary<string, AudioSource> m_dicAudio;
+
+	public void PlaySound(string soundName, eSoundPlayType type)
+	{
+		AudioSource audio = null;
+		if (m_dicAudio.ContainsKey (soundName)) {
+			audio = m_dicAudio [soundName];
+		}
+		if (audio != null) {
+			switch (type) {
+			case eSoundPlayType.OneShot:
+				if (!audio.isPlaying) {
+					audio.Play ();
+				}
+				break;
+			case eSoundPlayType.Loop:
+				audio.loop = true;
+				audio.Play ();
+				break;
+			}
+		}
+	}
+
+	public void StopSound(string soundName)
+	{
+		AudioSource audio = null;
+		if (m_dicAudio.ContainsKey (soundName)) {
+			audio = m_dicAudio [soundName];
+		}
+		if (audio != null) {
+			audio.Stop ();
+		}
+	}
+
+	public void AllStop()
+	{
+		for (Dictionary<string, AudioSource>.Enumerator i = m_dicAudio.GetEnumerator(); i.MoveNext();) {
+			i.Current.Value.Stop ();
+		}
+	}
+}
