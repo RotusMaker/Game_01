55402374569d6dcd74cbba36e9513a5588dc4a4f
RotusMakerJB <RotusMakerJB@gmail.com>
2017-05-25 04:23:46 +0900

    메쉬컴바인적용

---
+++ Assets/UnResources/Script/Editor/MBMeshCombineEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(MBMeshCombine))]
+public class MBMeshCombineEditor : Editor {
+
+	MBMeshCombine myTarget;
+
+	private void OnEnable()
+	{
+		myTarget = (MBMeshCombine)target;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		myTarget.parent = (GameObject)EditorGUILayout.ObjectField("Mesh Parent", myTarget.parent, typeof(GameObject));
+
+		if (GUILayout.Button ("Combine")) {
+			myTarget.CombineMeshs();
+		}
+	}
+}
---
+++ Assets/UnResources/Script/MBMeshCombine.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections;
+
+public class MBMeshCombine : MonoBehaviour {
+	public GameObject parent = null;
+
+	public void CombineMeshs() {
+#if UNITY_EDITOR
+		Material material = null;
+		MeshFilter[] meshFilters = parent.GetComponentsInChildren<MeshFilter>();
+		MeshRenderer[] meshRenderers = parent.GetComponentsInChildren<MeshRenderer>();
+		CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+		int i = 0;
+		while (i < meshFilters.Length) {
+			combine[i].mesh = meshFilters[i].sharedMesh;
+			combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+			//combine[i].transform = meshFilters[i].transform.worldToLocalMatrix;
+			//meshFilters[i].gameObject.active = false;
+
+			if (material == null){
+				material = meshRenderers[i].sharedMaterial;
+			}
+
+			i++;
+		}
+		Mesh combineMesh = new Mesh();
+		combineMesh.CombineMeshes(combine);
+		combineMesh.name = parent.name;
+
+		Material combineMat = new Material (material);
+
+		string filePath = EditorUtility.SaveFilePanelInProject("Save Procedural Mesh", "Procedural Mesh", "asset", "");
+		AssetDatabase.CreateAsset(combineMat, filePath+".mat");
+		AssetDatabase.CreateAsset(combineMesh, filePath);
+		AssetDatabase.Refresh ();
+
+		GameObject prefab = new GameObject ("Prefab");
+		MeshFilter prefabMeshFilter = prefab.AddComponent<MeshFilter> ();
+		MeshRenderer prefabMeshRenderer = prefab.AddComponent<MeshRenderer> ();
+		prefabMeshFilter.mesh = combineMesh;
+		prefabMeshRenderer.castShadows = false;
+		prefabMeshRenderer.receiveShadows = false;
+		prefabMeshRenderer.material = combineMat;
+
+		PrefabUtility.CreatePrefab (filePath+".prefab", prefab);
+		//AssetDatabase.SaveAssets();
+
+		//DestroyImmediate (prefab);
+#endif
+	}
+}
